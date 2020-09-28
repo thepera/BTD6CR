1e591c096c2523161fa4129465b02ab1706d53e0
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-09-28 20:43:31 +0800

    updated all the maps, towers, and heroes

---
+++ js/json/jsonVar.js
@@ -0,0 +1,469 @@
+let towers = {
+    primary: [
+        {
+            id: 0,
+            name: 'Dart Monkey',
+            icon: 'img/monkeys/dart.png',
+            camo: true,
+        },
+        {
+            id: 1,
+            name: 'Boomerang Monkey',
+            icon: 'img/monkeys/rang.png',
+            camo: false,
+        },
+        {
+            id: 2,
+            name: 'Bomb Shooter',
+            icon: 'img/monkeys/bomb.png',
+            camo: false,
+        },
+        {
+            id: 3,
+            name: 'Tack Shooter',
+            icon: 'img/monkeys/tack.png',
+            camo: false,
+        },
+        {
+            id: 4,
+            name: 'Ice Monkey',
+            icon: 'img/monkeys/ice.png',
+            camo: false,
+        },
+        {
+            id: 5,
+            name: 'Glue Gunner',
+            icon: 'img/monkeys/glue.png',
+            camo: false,
+        },
+    ],
+    military: [
+        {
+            id: 0,
+            name: 'Sniper Monkey',
+            icon: 'img/monkeys/sniper.png',
+            camo: true,
+        },
+        {
+            id: 1,
+            name: 'Monkey Sub',
+            icon: 'img/monkeys/sub.png',
+            camo: true,
+        },
+        {
+            id: 2,
+            name: 'Monkey Buccaneer',
+            icon: 'img/monkeys/buccaneer.png',
+            camo: true,
+        },
+        {
+            id: 3,
+            name: 'Monkey Ace',
+            icon: 'img/monkeys/ace.png',
+            camo: true,
+        },
+        {
+            id: 4,
+            name: 'Heli Pilot',
+            icon: 'img/monkeys/heli.png',
+            camo: true,
+        },
+    ],
+    magic: [
+        {
+            id: 0,
+            name: 'Wizard Monkey',
+            icon: 'img/monkeys/wizard.png',
+            camo: true,
+        },
+        {
+            id: 1,
+            name: 'Super Monkey',
+            icon: 'img/monkeys/super.png',
+            camo: true,
+        },
+        {
+            id: 2,
+            name: 'Ninja Monkey',
+            icon: 'img/monkeys/ninja.png',
+            camo: true,
+        },
+        {
+            id: 3,
+            name: 'Alchemist',
+            icon: 'img/monkeys/alchemist.png',
+            camo: false,
+        },
+        {
+            id: 4,
+            name: 'Druid',
+            icon: 'img/monkeys/druid.png',
+            camo: false,
+        },
+    ],
+    support: [
+        {
+            id: 0,
+            name: 'Banana Farm',
+            icon: 'img/monkeys/farm.png',
+            camo: false,
+        },
+        {
+            id: 1,
+            name: 'Spike Factory',
+            icon: 'img/monkeys/factory.png',
+            camo: true,
+        },
+        {
+            id: 2,
+            name: 'Monkey Village',
+            icon: 'img/monkeys/village.png',
+            camo: true,
+        },
+
+        {
+            id: 4,
+            name: 'Engineer Monkey',
+            camo: true,
+        },
+    ],
+};
+
+let heroes = [
+    {
+        id: 0,
+        name: 'Quincy',
+        icon: 'img/heroes/quincy.png',
+    },
+    {
+        id: 1,
+        name: 'Gwendolin',
+        icon: 'img/heroes/gwendolin.png',
+    },
+    {
+        id: 2,
+        name: 'Striker Jones',
+        icon: 'img/heroes/jones.png',
+    },
+    {
+        id: 3,
+        name: 'Obyn Greenfoot',
+        icon: 'img/heroes/obyn.png',
+    },
+    {
+        id: 4,
+        name: 'Capitan Churchill',
+        icon: 'img/heroes/churchill.png',
+    },
+    {
+        id: 5,
+        name: 'Benjamin',
+    },
+    {
+        id: 6,
+        name: 'Captain Churchill',
+    },
+    {
+        id: 7,
+        name: 'Pat Fusty',
+    },
+    {
+        id: 8,
+        name: 'Ezili',
+    },
+    {
+        id: 9,
+        name: 'Adora',
+    },
+    {
+        id: 10,
+        name: 'Admiral Brickell',
+    },
+    {
+        id: 11,
+        name: 'Etienne',
+    },
+];
+
+let maps = {
+    beginner: [
+        {
+            id: 0,
+            name: 'Monkey meadow',
+            icon: 'img/maps/meadow.png',
+        },
+        {
+            id: 1,
+            name: 'Tree stump',
+            icon: 'img/maps/stump.png',
+        },
+        {
+            id: 2,
+            name: 'Town center',
+            icon: 'img/maps/town.png',
+        },
+        {
+            id: 3,
+            name: 'In the loop',
+            icon: 'img/maps/loop.png',
+        },
+        {
+            id: 4,
+            name: 'Cubism',
+            icon: 'img/maps/cubism.png',
+        },
+        {
+            id: 5,
+            name: 'Four circles',
+            icon: 'img/maps/circles.png',
+        },
+        {
+            id: 6,
+            name: 'Hedge',
+            icon: 'img/maps/hedge.png',
+        },
+        {
+            id: 7,
+            name: 'End of the road',
+            icon: 'img/maps/road.png',
+        },
+        {
+            id: 8,
+            name: 'Logs',
+            icon: 'img/maps/logs.png',
+        },
+        {
+            id: 9,
+            name: 'Lotus Island',
+        },
+        {
+            id: 10,
+            name: 'Candy Falls',
+        },
+        {
+            id: 11,
+            name: 'Winter Park',
+        },
+        {
+            id: 12,
+            name: 'Carved',
+        },
+        {
+            id: 13,
+            name: 'Park Path',
+        },
+    ],
+    intermediate: [
+        {
+            id: 0,
+            name: 'Firing range',
+            icon: 'img/maps/range.png',
+        },
+        {
+            id: 1,
+            name: 'Cracked',
+            icon: 'img/maps/cracked.png',
+        },
+        {
+            id: 2,
+            name: 'Streambed',
+            icon: 'img/maps/streambed.png',
+        },
+        {
+            id: 3,
+            name: 'Chutes',
+            icon: 'img/maps/chutes.png',
+        },
+        {
+            id: 4,
+            name: 'Rake',
+            icon: 'img/maps/rake.png',
+        },
+        {
+            id: 5,
+            name: 'Spice islands',
+            icon: 'img/maps/spice.png',
+        },
+        {
+            id: 6,
+            name: 'Downstream',
+            icon: 'img/maps/downstream.png',
+        },
+        {
+            id: 7,
+            name: 'Bazaar',
+        },
+        {
+            id: 8,
+            name: "Adora's temple",
+        },
+        {
+            id: 9,
+            name: 'Spring Spring',
+        },
+        {
+            id: 10,
+            name: 'Moon Landing',
+        },
+    ],
+    advanced: [
+        {
+            id: 0,
+            name: 'Anotherbrick',
+            icon: 'img/maps/brick.png',
+        },
+        {
+            id: 1,
+            name: 'Off the coast',
+            icon: 'img/maps/coast.png',
+        },
+        {
+            id: 2,
+            name: 'Cornfield',
+            icon: 'img/maps/corn.png', // Why do americans call this "maze"? It makes no sense to me. inb4: Too lazy to Google.
+        },
+        {
+            id: 3,
+            name: 'Underground',
+            icon: 'img/maps/underground.png',
+        },
+        {
+            id: 4,
+            name: 'High finance',
+            icon: 'img/maps/finance.png',
+        },
+        {
+            id: 5,
+            name: 'Mesa',
+        },
+        {
+            id: 6,
+            name: 'Geared',
+        },
+        {
+            id: 7,
+            name: 'Spillway',
+        },
+        {
+            id: 8,
+            name: 'Cargo',
+        },
+        {
+            id: 9,
+            name: "Pat's pond",
+        },
+        {
+            id: 10,
+            name: 'Peninsula',
+        },
+    ],
+    expert: [
+        {
+            id: 0,
+            name: 'Muddy puddles',
+            icon: 'img/maps/mud.png',
+        },
+        {
+            id: 1,
+            name: 'Ouch',
+            icon: 'img/maps/ouch.png',
+        },
+        {
+            id: 2,
+            name: 'Dark Castle',
+        },
+        {
+            id: 3,
+            name: 'Quad',
+        },
+        {
+            id: 4,
+            name: 'Flooded Valley',
+        },
+        {
+            id: 5,
+            name: 'Infernal',
+        },
+        {
+            id: 6,
+            name: 'Bloody Puddles',
+        },
+        {
+            id: 7,
+            name: 'Workshop',
+        },
+    ],
+};
+
+let difficulties = [
+    {
+        id: 0,
+        name: 'Easy',
+        icon: 'img/mode/Easy.png',
+    },
+    {
+        id: 1,
+        name: 'Primary only',
+        icon: 'img/mode/primary.png',
+    },
+    {
+        id: 2,
+        name: 'Deflation',
+        icon: 'img/mode/deflation.png',
+    },
+    {
+        id: 3,
+        name: 'Normal',
+        icon: 'img/mode/normal.png',
+    },
+    {
+        id: 4,
+        name: 'Military Only',
+        icon: 'img/mode/military.png',
+    },
+    {
+        id: 5,
+        name: 'Apopalypse',
+        icon: 'img/mode/apopalypse.png',
+    },
+    {
+        id: 6,
+        name: 'Reverse',
+        icon: 'img/mode/reverse.png',
+    },
+    {
+        id: 7,
+        name: 'Hard',
+        icon: 'img/mode/hard.png',
+    },
+    {
+        id: 8,
+        name: 'Magic only',
+        icon: 'img/mode/magic.png',
+    },
+    {
+        id: 9,
+        name: 'Double HP MOABS',
+        icon: 'img/mode/double.png',
+    },
+    {
+        id: 10,
+        name: 'Half cash',
+        icon: 'img/mode/half.png',
+    },
+    {
+        id: 11,
+        name: 'Chimps',
+        icon: 'img/mode/chimps.png',
+    },
+    {
+        id: 12,
+        name: 'Alternate bloons rounds',
+        icon: 'img/mode/alternate.png',
+    },
+    {
+        id: 13,
+        name: 'Impoppable',
+        icon: 'img/mode/impoppable.png',
+    },
+];
